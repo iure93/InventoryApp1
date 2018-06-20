dbb72960ee3216641194251cf77d162c502b2ab5
iure93 <33991424+iure93@users.noreply.github.com>
2018-06-20 18:29:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp1/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.android.inventoryapp1;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp1.data.BookDbHelper;
+
+import static com.example.android.inventoryapp1.data.BookContract.BookEntry;
+
+
+/**
+ * Displays list of books that were entered and stored in the app.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /** Database helper that will provide us access to the database */
+    private BookDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        mDbHelper = new BookDbHelper(this);
+
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the book database.
+     */
+    private void displayDatabaseInfo() {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM books"
+        // to get a Cursor that contains all rows from the books table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " +BookEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // books table in the database).
+            TextView displayView = (TextView) findViewById(R.id.text_view_book);
+            displayView.setText("Number of rows in books database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
+    /**
+     * Helper method to insert hardcoded book data into the database. For debugging purposes only.
+     */
+    private void insertBook() {
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and Odissea's book attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, "Odissea");
+        values.put(BookEntry.COLUMN_PRICE, "19.99");
+        values.put(BookEntry.COLUMN_QUANTITY, "2");
+        values.put(BookEntry.COLUMN_SUPPLIER_NAME,  "Vasta");
+        values.put(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER,  "0481582");
+
+        // Insert a new row for Toto in the database, returning the ID of that new row.
+        // The first argument for db.insert() is the pets table name.
+        // The second argument provides the name of a column in which the framework
+        // can insert NULL in the event that the ContentValues is empty (if
+        // this is set to "null", then the framework will not insert a row when
+        // there are no values).
+        // The third argument is the ContentValues object containing the info for Toto.
+        long newRowId = db.insert(BookEntry.TABLE_NAME, null, values);
+        Log.v("MainActivity", "New row ID" + newRowId);
+    }
+}
---
+++ app/src/main/java/com/example/android/inventoryapp1/data/BookContract.java
@@ -0,0 +1,64 @@
+package com.example.android.inventoryapp1.data;
+
+import android.provider.BaseColumns;
+
+public final class BookContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+
+    private BookContract() {}
+    /**
+     * Inner class that defines constant values for the books database table.
+     * Each entry in the table represents a single book.
+     */
+    public static final class BookEntry implements BaseColumns {
+
+        /** Name of database table for book */
+        public final static String TABLE_NAME = "books";
+
+        /**
+         * Unique ID number for the book (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="product name";
+
+        /**
+         * Price of the product.
+         *
+         * Type: double
+         */
+        public final static String COLUMN_PRICE = "price";
+
+        /**
+         * Quantity of the product.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_QUANTITY = "quantity";
+
+        /**
+         * Supplier name.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_SUPPLIER_NAME= "supplier name";
+
+        /**
+         * Supplier phone number.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_SUPPLIER_PHONE_NUMBER= "supplier phone number";
+
+    }
+
+}
